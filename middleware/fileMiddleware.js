2cd920e80c7e2b804c28a024c196f7dde95ac746
Alexander-Keyer <alex-keyer@mail.ru>
2022-04-17 18:41:08 +0300

    NADO

--- middleware/fileMiddleware.js
+++ middleware/fileMiddleware.js
@@ -2,20 +2,10 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination(req, file, cb) {
-    try {
-      console.log('------------------')
-      cb(null, 'users-files/')
-    } catch(e) {
-      console.log(e)
-    }
+    cb(null, 'users-files/')
   },
   filename(req, file, cb) {
-    try {
-      console.log('------------------')
-      cb(null, new Date().toISOString().split(':').join('.') + "--" + file.originalname)
-    } catch(e) {
-      console.log(e)
-    }
+    cb(null, new Date().toISOString().split(':').join('.') + "--" + file.originalname)
   }
 })
 
@@ -23,12 +13,7 @@ const storage = multer.diskStorage({
 
 const fileFilter = (req, file, cb) => {
   // if (types.includes(file.mimetype)) {
-  try {
-    console.log('------------------')
     cb(null, true)
-  } catch(e) {
-    console.log(e)
-  }
   // } else {
   //   cb(null, false)
   // }
