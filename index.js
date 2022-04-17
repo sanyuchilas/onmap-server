693d4b6056115a58604f31302df1ccc93295073f
Alexander-Keyer <alex-keyer@mail.ru>
2022-04-17 15:48:14 +0300

    NADO

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/routes')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
-const add = require('./add')
+// const add = require('./add')
 
 const PORT = process.env.PORT || 5000
 
@@ -33,4 +33,4 @@ const start = async () => {
 
 start()
 
-add.add()
+// add.add()
