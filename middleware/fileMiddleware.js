7b295bf21003a5053402d3560d19e5eea0d6e491
Alexander-Keyer <alex-keyer@mail.ru>
2022-04-17 10:28:39 +0300

    FIX FILES

--- middleware/fileMiddleware.js
+++ middleware/fileMiddleware.js
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
   }
 })
 
-const types = ['image/png', 'image/jpeg', 'image/jpg']
+// const types = ['image/png', 'image/jpeg', 'image/jpg']
 
 const fileFilter = (req, file, cb) => {
   // if (types.includes(file.mimetype)) {
